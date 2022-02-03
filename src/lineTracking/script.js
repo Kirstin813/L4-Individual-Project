dacca241397fe5371897830c5dd0a4b85f7aeeec
Kirstin813 <k.ritchie123@icloud.com>
2022-02-03 19:58:38 +0000

    Adding css styling to fix canvas position

--- src/lineTracking/script.js
+++ src/lineTracking/script.js
@@ -67,13 +67,13 @@
     function moveRobot(centreSensor, leftSensor, rightSensor) {
 
       if (centreSensor[0] < 60) {
-        console.log("robot moving straight");
+        //console.log("robot moving straight");
         move(forward);
       } else if (leftSensor[0] < 60) {
-        console.log("robot moving left");
+        //console.log("robot moving left");
         move(left);
       } else if (rightSensor[0] < 60) {
-        console.log("robot moving right");
+        //console.log("robot moving right");
         move(right);
       }
     }
