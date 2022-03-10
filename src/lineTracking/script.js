6cf24b79e17ad87febd34c7dc1b05d1af041a76b
Kirstin813 <k.ritchie123@icloud.com>
2022-03-10 20:19:04 +0000

    Trying a different way to switch camera to fix issue

--- src/lineTracking/script.js
+++ src/lineTracking/script.js
@@ -1,130 +1,123 @@
-(function () {
+(() => {
+  const videoElm = document.querySelector('#video');
+  const btnFront = document.querySelector('#btn-front');
+  const btnBack = document.querySelector('#btn-back');
+  const canvas = document.getElementById("canvas");
+  var context = canvas.getContext('2d');
+  let currentAction;
 
-  /* Checking if the media devices is suitable for the camera api */
-    if (
-      !"mediaDevices" in navigator ||
-      !"getUserMedia" in navigator.mediaDevices
-    ) {
-      alert("Camera API is not available in your browser");
-      return;
-    }
-  
-    // Canvas and Video elements
-    const video = document.querySelector("#video");
-    const btnChangeCamera = document.querySelector("#btnChangeCamera");
-    const btnGreyscale = document.querySelector("#btnGreyscale");
-    const btnConnect = document.querySelector('#btnConnect');
-    const btnDisconnect = document.querySelector('#btnDisconnect');
-    const btnStop = document.querySelector('#btnStop');
-    const canvas = document.getElementById("canvas");
-    var context = canvas.getContext('2d');
+  const supports = navigator.mediaDevices.getSupportedConstraints();
+  if (!supports['facingMode']) {
+    alert('Browser Not supported!');
+    return;
+  }
 
-    // Variable for the current action the robot is taking 
-    let currentAction;
-  
-    
-    // video constraints
-    const constraints = {
+  let stream;
+
+  const capture = async facingMode => {
+    const options = {
+      audio: false,
       video: {
-        width: 500, 
-        height: 360
+        facingMode,
       },
     };
-  
 
-    // default true (front facing camera), false (back facing camera)
-    let useFrontCamera = true;
-  
-    // current video stream
-    let videoStream;
-  
-    // switch camera
-    btnChangeCamera.addEventListener("click", function () {
-      useFrontCamera = !useFrontCamera;
-  
-      initializeCamera();
-    });
-
-    // displaying the canvas feed 
-    btnGreyscale.addEventListener("click", function() {
-      video.play();
-    
-      drawFrame(video);
-
-    });
-
-    // connecting to the robot
-    btnConnect.addEventListener("click", function() {
-      connect();
-    });
-
-    // disconnecting from the robot 
-    btnDisconnect.addEventListener("click", function() {
-      disconnect();
-    });
-    
-    // stop the robot moving 
-    btnStop.addEventListener("click", function() {
-      stop();
-    });
-
-    /* Function to move/update the moving action of the robot */
-    function move(action) {
-      if (action != currentAction) {
-        currentAction = action;
-        currentAction();
+    try {
+      if (stream) {
+        const tracks = stream.getTracks();
+        tracks.forEach(track => track.stop());
       }
+      stream = await navigator.mediaDevices.getUserMedia(options);
+    } catch (e) {
+      alert(e);
+      return;
     }
-    
-    /* Conditionals for each sensor signalling which direction to move the robot */
-    function moveRobot(centreSensor, leftSensor, rightSensor) {
+    videoElm.srcObject = null;
+    videoElm.srcObject = stream;
+    videoElm.play();
+  }
 
-      //console.log(centreSensor);
-      if (centreSensor < 90) {
-        console.log("robot moving forward");
-        move(forward);
-      } else if (leftSensor < 90) {
-        console.log("robot moving left");
-        move(left);
-      } else if (rightSensor < 90) {
-        console.log("robot moving right");
-        move(right);
-      }
+  btnBack.addEventListener('click', () => {
+    capture('environment');
+
+    draw(videoElm);
+  });
+
+  btnFront.addEventListener('click', () => {
+    capture('user');
+
+    draw(videoElm);
+  });
+
+   // connecting to the robot
+  btnConnect.addEventListener("click", function() {
+    connect();
+  });
+
+  // disconnecting from the robot 
+  btnDisconnect.addEventListener("click", function() {
+    disconnect();
+  });
+  
+  // stop the robot moving 
+  btnStop.addEventListener("click", function() {
+    stop();
+  });
+
+  /* Function to move/update the moving action of the robot */
+  function move(action) {
+    if (action != currentAction) {
+      currentAction = action;
+      currentAction();
     }
-
-    // Function returning the average integer of the pixel array 
-    function getAverage(pixels) {
-
-      const pixelArr = [];
-      const average = arr => arr.reduce((a,b) => a + b, 0) / arr.length;
-
-      for (let i = 0; i < pixels.data.length; i = i+4) {
-        pixelArr.push(pixels.data[i])
-      }
-
-      // returns average integer to 2 decimal places
-      return average(pixelArr).toFixed(2);
-
-    }
-
-    function drawFrame(video) {
-
-      canvas.width = video.videoWidth;
-      canvas.height = video.videoHeight;
-
-      // converting the canvas into greyscale filter 
-      context.filter = 'grayscale(1)';
-
-      context.drawImage(video, 0, 0);
+  }
       
-      /**
-       * Retrieving pixel data from the bottom section of the canvas 
-       * Each pixel array will act as sensors to detect the black line 
-       */
+  /* Conditionals for each sensor signalling which direction to move the robot */
+  function moveRobot(centreSensor, leftSensor, rightSensor) {
+  
+    //console.log(centreSensor);
+    if (centreSensor < 90) {
+      console.log("robot moving forward");
+      move(forward);
+    } else if (leftSensor < 90) {
+      console.log("robot moving left");
+      move(left);
+    } else if (rightSensor < 90) {
+      console.log("robot moving right");
+      move(right);
+    }
+  }
+
+   // Function returning the average integer of the pixel array 
+   function getAverage(pixels) {
+
+    const pixelArr = [];
+    const average = arr => arr.reduce((a,b) => a + b, 0) / arr.length;
+
+    for (let i = 0; i < pixels.data.length; i = i+4) {
+      pixelArr.push(pixels.data[i])
+    }
+
+    // returns average integer to 2 decimal places
+    return average(pixelArr).toFixed(2);
+
+  }
+
+  function draw(videoElm) {
+
+    canvas.width = videoElm.videoWidth;
+    canvas.height = videoElm.videoHeight;
+
+
+    // converting the canvas into greyscale filter 
+    context.filter = 'grayscale(1)';
+
+    context.drawImage(videoElm, 0, 0);
+
+    if (canvas.height > 0) {
       var left = getAverage(context.getImageData(0, canvas.height - 50, (canvas.width / 3), 50));
       var centre = getAverage(context.getImageData((canvas.width / 3), canvas.height - 50, (canvas.width / 3), 50));
       var right = getAverage(context.getImageData(2 * (canvas.width / 3), canvas.height - 50, (canvas.width / 3), 50));
-      
 
       /* Outline the sensors in a white rectangle border */
       context.strokeStyle = 'white';
@@ -136,139 +129,115 @@
 
       // Calling function to start moving the robot using the retrieved sensors 
       moveRobot(centre, left, right);
- 
-      setTimeout(function () {
-        drawFrame(video);
-      }, 10);
     }
 
+    setTimeout(function () {
+      draw(videoElm);
+    }, 10);
+  }
 
-    // connection variable to store the state of the connection
-    let connection;
+  // connection variable to store the state of the connection
+  let connection;
 
-    function onLine(lineString) {
-      console.log(lineString.trim());
-    }
-
-    function connect() {
-      if (connection) {
-        disconnect()
-      }
+  function onLine(lineString) {
+    console.log(lineString.trim());
+  }
   
-      UART.connect(function(c) {
-        if (!c) {
-          console.log("Couldn't connect!");
-          return;
-        }
-        connection = c;
-        // Handle the data we get back, and call 'onLine'
-        // whenever we get a line
-        var buf = "";
-        connection.on("data", function(d) {
-          buf += d;
-          var i = buf.indexOf("\n");
-          while (i>=0) {
-            onLine(buf.substr(0,i));
-            buf = buf.substr(i+1);
-            i = buf.indexOf("\n");
-          }
-        });
-      });
-    }
-
-    function disconnect() {
-      if (connection) {
-        connection.close();
-        connection = undefined;
-      }
-    }
-
-  /**
-    * Following functions implement the actions the robot can take 
-    *  Forward
-    *  Backward
-    *  Left
-    *  Right
-    *  Stop
-  */
-
-    function forward() {
-      if (connection) {
-        connection.write('forward();\n');
-      }
-    }       
-
-    function backward() {
-      if (connection) {
-        connection.write('backward();\n');
-      }
-    }
-
-    function left() {
-      if (connection) {
-        connection.write('left();\n');
-      }
-    }
-
-    function right() {
-      if (connection) {
-        connection.write('right();\n');
-      }
-    }
-
-    function stop() {
-      if (connection) {
-        connection.write("stop();\n");
-      }
-    }
-
-    // Get the modal
-    var modal = document.getElementById("myModal");
-
-    // Get the button that opens the modal
-    var btn = document.getElementById("myBtn");
-
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
-
-    // When the user clicks the button, open the modal 
-    btn.onclick = function() {
-      modal.style.display = "block";
-    }
-
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-      modal.style.display = "none";
-    }
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-      if (event.target == modal) {
-        modal.style.display = "none";
-      }
+  function connect() {
+    if (connection) {
+      disconnect()
     }
     
-    // Function to stop the video stream
-    function stopVideoStream() {
-      if (videoStream) {
-        videoStream.getTracks().forEach((track) => {
-          track.stop();
-        });
+    UART.connect(function(c) {
+      if (!c) {
+        console.log("Couldn't connect!");
+        return;
       }
+      connection = c;
+      // Handle the data we get back, and call 'onLine'
+      // whenever we get a line
+      var buf = "";
+      connection.on("data", function(d) {
+        buf += d;
+        var i = buf.indexOf("\n");
+        while (i>=0) {
+          onLine(buf.substr(0,i));
+          buf = buf.substr(i+1);
+          i = buf.indexOf("\n");
+        }
+      });
+    });
+  }
+  
+  function disconnect() {
+    if (connection) {
+      connection.close();
+      connection = undefined;
     }
+  }
   
-    // initialize
-    async function initializeCamera() {
-      stopVideoStream();
-      constraints.video.facingMode = useFrontCamera ? "user" : "environment";
+    /**
+      * Following functions implement the actions the robot can take 
+      *  Forward
+      *  Backward
+      *  Left
+      *  Right
+      *  Stop
+    */
   
-      try {
-        videoStream = await navigator.mediaDevices.getUserMedia(constraints);
-        video.srcObject = videoStream;
-      } catch (err) {
-        alert("Could not access the camera");
-      }
+  function forward() {
+    if (connection) {
+      connection.write('forward();\n');
     }
+  }       
   
-    initializeCamera();
-  })();
+  function backward() {
+    if (connection) {
+      connection.write('backward();\n');
+    }
+  }
+  
+  function left() {
+    if (connection) {
+      connection.write('left();\n');
+    }
+  }
+  
+  function right() {
+    if (connection) {
+      connection.write('right();\n');
+    }
+  }
+  
+  function stop() {
+    if (connection) {
+      connection.write("stop();\n");
+    }
+  }
+  
+  // Get the modal
+  var modal = document.getElementById("myModal");
+  
+  // Get the button that opens the modal
+  var btn = document.getElementById("myBtn");
+  
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+  
+  // When the user clicks the button, open the modal 
+  btn.onclick = function() {
+    modal.style.display = "block";
+  }
+  
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+  
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+})();
