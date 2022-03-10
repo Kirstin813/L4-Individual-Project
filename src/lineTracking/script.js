5adda07affbd6e18100bc2a866458fe954b17422
Kirstin813 <k.ritchie123@icloud.com>
2022-03-10 18:43:54 +0000

    Reverting back to original code

--- src/lineTracking/script.js
+++ src/lineTracking/script.js
@@ -22,11 +22,12 @@
     // Variable for the current action the robot is taking 
     let currentAction;
   
+    
     // video constraints
     const constraints = {
       video: {
-        width: 540, 
-        height: 380
+        width: 500, 
+        height: 360
       },
     };
   
@@ -218,6 +219,32 @@
     function stop() {
       if (connection) {
         connection.write("stop();\n");
+      }
+    }
+
+    // Get the modal
+    var modal = document.getElementById("myModal");
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks the button, open the modal 
+    btn.onclick = function() {
+      modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
       }
     }
     
